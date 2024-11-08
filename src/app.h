1586def367b5e3b76be32131625825729623fafe
henrypp <sforce5@mail.ru>
2024-11-08 22:32:37 +0500

    add id header

---
+++ src/app.h
@@ -0,0 +1,11 @@
+// Autoruns2
+// Copyright (c) 2025 Henry++
+
+#pragma once
+
+#define APP_NAME L"Autoruns2"
+#define APP_NAME_SHORT L"autoruns2"
+#define APP_VERSION L"1.0"
+#define APP_VERSION_RES 1,0,0,0
+#define APP_AUTHOR L"Henry++"
+#define APP_COPYRIGHT L"(c) 2025 " APP_AUTHOR L". All Rights Reversed."
