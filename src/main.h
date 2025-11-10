a34bcb05a09de0aa6f06c11e5886bd3001be3529
henrypp <sforce5@mail.ru>
2025-11-11 02:45:30 +0500

    upd project configuration

--- src/main.h
+++ src/main.h
@@ -16,24 +16,6 @@
 #pragma comment(lib, "crypt32.lib")
 #pragma comment(lib, "wintrust.lib")
 
-// CryptCATAdminAcquireContext2 (win8+)
-typedef BOOL (WINAPI *CCAAC2)(
-	_Out_ PHANDLE hcat_admin,
-	_In_opt_ LPCGUID pgSubsystem,
-	_In_opt_ PCWSTR pwszHashAlgorithm,
-	_In_opt_ PCCERT_STRONG_SIGN_PARA pStrongHashPolicy,
-	_Reserved_ DWORD dwFlags
-	);
-
-// CryptCATAdminCalcHashFromFileHandle2 (win8+)
-typedef BOOL (WINAPI *CCAHFFH2)(
-	_In_ HCATADMIN hCatAdmin,
-	_In_ HANDLE hFile,
-	_Inout_ DWORD *pcbHash,
-	_Out_writes_bytes_to_opt_ (*pcbHash, *pcbHash) BYTE *pbHash,
-	_Reserved_ DWORD dwFlags
-	);
-
 // ui
 #define LANG_SUBMENU 1
 #define LANG_MENU 6
@@ -65,56 +47,60 @@ typedef BOOL (WINAPI *CCAHFFH2)(
 #define SCAN_OPTION_DEFAULT (SCAN_OPTION_HIJACKS | SCAN_OPTION_WEB | SCAN_OPTION_PENDINGFRO | SCAN_OPTION_CODECS | SCAN_OPTION_KNOWNDLLS | SCAN_OPTION_TASKSCHEDULER | SCAN_OPTION_SERVICES | SCAN_OPTION_DRIVERS)
 
 // group id
-#define HkcuRun 0
-#define HkcuWow64Run 1
-#define HkcuRunOnce 2
-#define HkcuWow64RunOnce 3
-#define HkcuRunOnceEx 4
-#define HkcuRunServices 5
-#define HkcuRunServicesOnce 6
-#define HkcuPoliciesRun 7
-#define HklmRun 8
-#define HklmRunEx 9
-#define HklmWow64Run 10
-#define HklmRunOnce 11
-#define HklmWow64RunOnce 12
-#define HklmRunOnceEx 13
-#define HklmWow64RunOnceEx 14
-#define HklmRunServices 15
-#define HklmRunServicesOnce 16
-#define HklmPoliciesRun 17
-#define HklmTerminalRun 18
-#define HklmTerminalRunOnce 19
-#define HklmTerminalRunOnceEx 20
-#define StartupDir1 21
-#define StartupDir2 22
-#define HklmAlternateShell 23
-#define HklmSessionManager 24
-#define HklmIconServiceLib 25
-#define HklmWinlogon 26
-#define HkcuCommandProcessor 27
-#define HklmCommandProcessor 28
-#define HklmHijacks 29
-#define HklmHijacksWow64 30
-#define HklmSilentExitProcess 31
-#define HklmRdpwd 32
-#define HklmCodecsClsid1 34
-#define HklmCodecsClsid2 35
-#define HklmCodecsClsid3 37
-#define HklmCodecsClsid4 38
-#define HkcuCodecsClsid5 39
-#define HklmCodecsClsid6 40
-#define HkcuCodecs 41
-#define HklmCodecs 42
-#define HklmCodecsWow64 43
-#define HklmFontDrivers 44
-#define HklmBHO 45
-#define HklmBHOWow64 46
-#define HkcuUrlSearchHooks 47
-#define HklmKnownDlls 48
-#define TaskScheduler 49
-#define Services 50
-#define Drivers 51
+typedef enum _GROUP_ID
+{
+	HkcuRun,
+	HkcuWow64Run,
+	HkcuRunOnce,
+	HkcuWow64RunOnce,
+	HkcuRunOnceEx,
+	HkcuRunServices,
+	HkcuRunServicesOnce,
+	HkcuPoliciesRun,
+	HklmRun,
+	HklmRunEx,
+	HklmWow64Run,
+	HklmRunOnce,
+	HklmWow64RunOnce,
+	HklmRunOnceEx,
+	HklmWow64RunOnceEx,
+	HklmRunServices,
+	HklmRunServicesOnce,
+	HklmPoliciesRun,
+	HklmTerminalRun,
+	HklmTerminalRunOnce,
+	HklmTerminalRunOnceEx,
+	StartupDir1,
+	StartupDir2,
+	HklmAlternateShell,
+	HklmSessionManager,
+	HklmIconServiceLib,
+	HklmWinlogon,
+	HkcuCommandProcessor,
+	HklmCommandProcessor,
+	HklmHijacks,
+	HklmHijacksWow64,
+	HklmSilentExitProcess,
+	HklmRdpwd,
+	HkcuCodecs,
+	HklmCodecs,
+	HklmCodecsClsid1,
+	HklmCodecsClsid2,
+	HklmCodecsClsid3,
+	HklmCodecsClsid4,
+	HkcuCodecsClsid5,
+	HklmCodecsClsid6,
+	HklmCodecsWow64,
+	HklmFontDrivers,
+	HklmBHO,
+	HklmBHOWow64,
+	HkcuUrlSearchHooks,
+	HklmKnownDlls,
+	TaskScheduler_,
+	Services,
+	Drivers,
+	GroupMax,
+} GROUP_ID,*PGROUP_ID;
 
 typedef struct _STATIC_DATA
 {
@@ -128,6 +114,7 @@ typedef struct _STATIC_DATA
 	HWND htoolbar;
 	HWND hsearchbar;
 	LONG64 start_time;
+	LONG icon_id;
 	volatile LONG total_count;
 	volatile LONG lock;
 } STATIC_DATA, *PSTATIC_DATA;
@@ -168,18 +155,18 @@ typedef enum _ITEM_TYPE
 
 typedef struct _ITEM_CONTEXT
 {
-	WCHAR reg_path[128];
 	PR_STRING signature_info;
 	PR_STRING version_info;
 	PR_STRING arguments;
 	PR_STRING file_path;
+	PR_STRING key_path;
 	PR_STRING lnk_path;
 	PR_STRING clsid;
 	PR_STRING name;
 	HANDLE hroot;
 	HWND hwnd;
 	ITEM_TYPE type;
-	LONG group_id;
+	GROUP_ID group_id;
 	LONG icon_id;
 	BOOLEAN is_hidden;
 	BOOLEAN is_loaded;
